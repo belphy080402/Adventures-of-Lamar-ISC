7e63d6a8ee1a6900ac66c38bb0d7e2e0d2ff784d
César Augusto Lacerda <78516304+Shiir@users.noreply.github.com>
2021-04-29 08:31:30 -0300

    Menu levando para o jogo

--- AdvLamar.s
+++ AdvLamar.s
@@ -5,19 +5,22 @@
 # Por favor, comente o codigo com clareza
 
 .data
+#macros
+.include "macro.s"
+
 # personagem para teste
-.include "lamar.data"
+.include "./Imagens/lamar.data"
 
 # mapa para teste
-.include "MAPA1.data"
+.include "./Imagens/MAPA1.data"
 
 # macro para o menu funcional
 .include "menu.s"
 
 .text
+Menu
 MAIN:
 
 # Ate o momento so mostra as imagens em sequencia.
 # So vou colocar a musica depois que estiver tudo
 # funcionando pra facilitar o teste.
-Menu
